c43ff0c09e1c85cb4c69dc5385c76e03f44a1053
Marius Mandal <mariusmandal@gmail.com>
2019-10-07 19:19:37 -0700

    UKMlog bruker APIv3
    Ikke implementert rapport-logg enda

--- ukmlog.php
+++ ukmlog.php
@@ -9,72 +9,38 @@ Author URI: http://www.ukm-norge.no
 */
 date_default_timezone_set('Europe/Oslo');
 
-require_once('UKM/monstring.class.php');
-require_once('UKM/inc/ukmlog.inc.php');
+require_once('UKM/Autoloader.php');
 
+class UKMlog extends UKMNorge\Wordpress\Modul {
+    public static $action = 'home';
+    public static $path_plugin = null;
 
-## HOOK MENU AND SCRIPTS
-if(is_admin()) {
-	add_action('admin_menu', 'UKMlog_menu', 400);
+    public static function hook() {
+        add_action('admin_menu', ['UKMlog','meny'], 400);
+    }
+
+    public static function meny() {
+        $page = add_submenu_page(
+            'UKMmonstring',
+            'Logg',
+            'Logg',
+            'editor',
+            'UKMlog',
+            ['UKMlog','renderAdmin']
+        );
+
+        add_action(
+            'admin_print_styles-' . $page,
+            ['UKMlog','scripts_and_styles']
+        );
+    }
+
+    public static function scripts_and_styles() {
+        wp_enqueue_script('WPbootstrap3_js');
+        wp_enqueue_style('WPbootstrap3_css');
+    }
 }
 
-## CREATE A MENU
-function UKMlog_menu() {
-
-	$page_log = add_submenu_page(
-		'UKMmonstring',
-		'Logg',
-		'Logg',
-		'editor',
-		'UKMlog_gui',
-		'UKMlog_gui'
-	);
-	
-	$page_log_innslag = add_submenu_page(
-		'UKMmonstring',
-		'Logg for innslag',
-		'Logg for innslag',
-		'editor',
-		'UKMlog_band',
-		'UKMlog_band'
-	);
-
-	add_action(
-		'admin_print_styles-' . $page_log,
-		'UKMlog_scripts_and_styles'
-	);
-	add_action(
-		'admin_print_styles-' . $page_log_innslag,
-		'UKMlog_scripts_and_styles'
-	);
-}
-
-function UKMlog_scripts_and_styles(){
-	wp_enqueue_style( 'UKMlogg_style', WP_PLUGIN_URL .'/UKMlog/style.logg.css');
-}
-
-
-function UKMlog_gui() {
-	$place = new monstring(get_option('pl_id'));
-
-	echo '<h3>Logg for '.$place->g('pl_name').'</h3>';
-
-	$logg = new SQL("SELECT `log_id` FROM `log_log`	
-					WHERE `log_pl_id` = '#plid'
-					ORDER BY `log_id` DESC",
-				array('plid'=>$place->g('pl_id')));
-
-	$logg = $logg->run();
-	while($row = SQL::fetch($logg)){
-		echo UKMlog_read($row['log_id']);
-	}
-}
-
-function UKMlog_rapport_statistikk(){
-	require_once('gui_rapporter.logg.php');
-}
-
-function UKMlog_band() {
-	require_once('gui_band.logg.php');
-}
+UKMlog::init(__DIR__);
+UKMlog::hook();
 ?>
